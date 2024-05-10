3dfc38a52f32bbbb9126fb7fddfc605014e67e33
Saif Saber <154082942+SaifSaberHussein@users.noreply.github.com>
2024-05-10 17:40:16 +0300

    Update Sign.js

--- Sign.js
+++ Sign.js
@@ -1,53 +1,15 @@
 
-document.addEventListener('DOMContentLoaded', function() {
-    const signupLinks = document.querySelectorAll('.login-link');
-
-    signupLinks.forEach(function(link) {
-        link.addEventListener('click', function(event) {
-            event.preventDefault();
-            const nextPage = link.getAttribute('href');
-            const currentPage = document.querySelector('.sign');
-
-            currentPage.classList.add('hide');
-
-            setTimeout(function() {
-                window.location.href = nextPage;
-            }, 500);
-        });
-    });
-});
-
-
-// function handleSubmit(event) {
-//     event.preventDefault(); 
-
-    
-//     const firstName = document.querySelector('#first-name').value;
-//     const secondName = document.querySelector('#second-name').value;
-//     const dob = document.querySelector('#date').value;
-//     const gender = document.querySelector('#gender').value;
-//     const email = document.querySelector('#email').value;
-//     const password = document.querySelector('#password').value;
-
-
-//     const formData = {
-//         firstName: firstName,
-//         secondName: secondName,
-//         dob: dob,
-//         gender: gender,
-//         email: email,
-//         password: password
-//     };
-
-//     localStorage.setItem('formData', JSON.stringify(formData));
-// }
-
-// const form = document.querySelector('form');
-// form.addEventListener('submit', handleSubmit);
-
-
-
-
 document.querySelector(".Expand").addEventListener("click", function() {
     document.getElementById("ul").style.fontSize='35px';
+});
+
+document.querySelector(".translate").addEventListener("click", function() {
+    document.getElementById("ul").style.direction = 'rtl';
+    document.getElementById("li-1").style.direction = 'rtl';
+    document.getElementById("li-1").innerHTML = 
+    "الاسم: مارد وشوشني ";
+
+    document.getElementById("li-2").style.direction = 'rtl';
+    document.getElementById("li-2").innerHTML = 
+    " الاسم: شلبي سولوفان";
 });
